05c01f97327e4e7816d589d210e5cf766837c856
Sigurd Andersen <sigurdschelde@gmail.com>
2023-08-21 12:13:39 +0200

    made project

---
+++ ChinookSpring/src/main/java/experis/chinookspring/ChinookSpringApplication.java
@@ -0,0 +1,13 @@
+package experis.chinookspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinookSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChinookSpringApplication.class, args);
+    }
+
+}
