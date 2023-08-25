1acbea280354445249ecf1b6a42a8820acde579a
Alexander <91903660+AlexanderJonstrup@users.noreply.github.com>
2023-08-25 15:40:17 +0200

    Update 03_relationshipSuperheroPower.sql
    Added comment.

--- Assignment1/03_relationshipSuperheroPower.sql
+++ Assignment1/03_relationshipSuperheroPower.sql
@@ -1,3 +1,6 @@
+/* Creates SuperheroPowers table witg superhero id and power id which references the tables Superheroes and Power respectively. 
+* Additionally, the table has a composite primary key consisting of the superhero id and power id.
+:*/
 CREATE TABLE SuperheroPowers (
 	sh_id serial REFERENCES Superheroes,
 	power_id serial REFERENCES Power,
